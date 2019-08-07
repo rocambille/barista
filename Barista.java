c3a5e121299e2854df569903ac6dda3659b38723
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2019-08-07 14:37:24 +0200

    changed directory structure

--- Barista.java
+++ Barista.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 class Barista {
 
     public static void main(String[] args) {
+        String baseInputDirectoryName = "src/main/resources/templates/barista";
+        String baseOutputDirectoryName = "src/main/java";
+        String outputDirectoryName = baseOutputDirectoryName + "/controllers";
+
         System.out.println("Hello dear customer :)");
         System.out.println("Do you need a resource controller? Just tell the name of your resource:");
 
@@ -13,14 +17,15 @@ class Barista {
 
             String fileContent = new String(
                 Files.readAllBytes(
-                    Paths.get("templates/Controller.java")
+                    Paths.get(baseInputDirectoryName + "/Controller.java")
                 )
             );
 
             String newFileContent = fileContent.replace("${Resource}", resourceName);
 
             System.out.println("Writing " + resourceName + "Controller.java for you...");
-            Files.write(Paths.get(resourceName + "Controller.java"), newFileContent.getBytes());
+            Files.createDirectories(Paths.get(outputDirectoryName));
+            Files.write(Paths.get(outputDirectoryName + "/" + resourceName + "Controller.java"), newFileContent.getBytes());
         }
         catch(Exception e) {
             e.printStackTrace();
