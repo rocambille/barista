cfc4bef13e9e490550041c82d3a82ffc2d91f48a
cecil millerioux <cecil.millerioux@gmail.com>
2019-09-14 22:55:28 +0200

    add a browse method in the controller

--- src/main/resources/templates/barista/Controller.java
+++ src/main/resources/templates/barista/Controller.java
@@ -1,9 +1,12 @@
 package #####;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /* Import your repository there */
 /* Import your entity there */
@@ -13,8 +16,13 @@ class ${Resource}Controller {
     @Autowired
     private ${Resource}Repository ${resource}Repository;
 
-    @PostMapping("/####")
+    @PostMapping("/#####")
     public ${Resource} store(@ModelAttribute ${Resource} ${resource}) {
         return ${resource}Repository.save(${resource});
+    }
+
+    @GetMapping("/#####")
+    public List<${Resource}> browse() {
+        return ${Resource}Repository.findAll();
     }
 }
