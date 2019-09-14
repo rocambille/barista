7465cac370d40d7ac6eac91a74ac1ddf26ce5afb
cecil millerioux <cecil.millerioux@gmail.com>
2019-09-14 22:29:44 +0200

    add import for, Autowired, ModelAttribute, PostMapping

--- src/main/resources/templates/barista/Controller.java
+++ src/main/resources/templates/barista/Controller.java
@@ -1,8 +1,12 @@
 package #####;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /* Import your repository there */
+/* Import your entity there */
 
 @Controller
 class ${Resource}Controller {
