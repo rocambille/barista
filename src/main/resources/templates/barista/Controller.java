1ad5471ee3a0039cb9dcc5d3874f2437c7e6d3a0
cecil millerioux <cecil.millerioux@gmail.com>
2019-09-14 22:17:00 +0200

    add repository for the controlled entity

--- src/main/resources/templates/barista/Controller.java
+++ src/main/resources/templates/barista/Controller.java
@@ -2,6 +2,10 @@ package #####;
 
 import org.springframework.stereotype.Controller;
 
+/* Import your repository there */
+
 @Controller
 class ${Resource}Controller {
+    @Autowired
+    private ${Resource}Repository ${resource}Repository;
 }
