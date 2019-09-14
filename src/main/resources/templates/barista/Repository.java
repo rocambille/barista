569ccdc39a834dbef60dedcb4057daf0cb6feedc
cecil millerioux <cecil.millerioux@gmail.com>
2019-09-14 19:37:56 +0200

    add content in repository template

---
+++ src/main/resources/templates/barista/Repository.java
@@ -0,0 +1,9 @@
+package #####;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ${Repository}Repository extends JpaRepository<${Repository}, Long>{
+
+}
