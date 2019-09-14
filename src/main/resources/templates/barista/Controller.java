a4984130a95b7ceb6ff459dea809f26346dbc858
cecil millerioux <cecil.millerioux@gmail.com>
2019-09-14 22:25:54 +0200

    add PostMapping method on controller

--- src/main/resources/templates/barista/Controller.java
+++ src/main/resources/templates/barista/Controller.java
@@ -8,4 +8,9 @@ import org.springframework.stereotype.Controller;
 class ${Resource}Controller {
     @Autowired
     private ${Resource}Repository ${resource}Repository;
+
+    @PostMapping("/####")
+    public ${Resource} store(@ModelAttribute ${Resource} ${resource}) {
+        return ${resource}Repository.save(${resource});
+    }
 }
