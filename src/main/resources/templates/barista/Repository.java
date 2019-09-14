5d340124734502b3e5f2264b01b02e9e215e8390
cecil millerioux <cecil.millerioux@gmail.com>
2019-09-14 19:46:45 +0200

    add a comment for remember the import of entity in repository template

--- src/main/resources/templates/barista/Repository.java
+++ src/main/resources/templates/barista/Repository.java
@@ -2,6 +2,7 @@ package #####;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+/* Import your ${Repository} Entity there */
 
 @Repository
 public interface ${Repository}Repository extends JpaRepository<${Repository}, Long>{
