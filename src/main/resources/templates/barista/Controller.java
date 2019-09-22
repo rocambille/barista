75fb350a4a6b0ee4dbf1e8117c346ad1dc8ffae2
cecil millerioux <cecil.millerioux@gmail.com>
2019-09-22 09:50:37 +0200

    add import ui.model

--- src/main/resources/templates/barista/Controller.java
+++ src/main/resources/templates/barista/Controller.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
