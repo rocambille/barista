88810bb555f2dc05bde82c3d82c09b4dd4f2bbc0
nymvno <ayhankaplan@hotmail.de>
2019-09-26 21:50:18 +0200

    Made Barista.java DRY: Removed unnecessary variables Added check for upper case letters for passed class names

--- Barista.java
+++ Barista.java
@@ -1,17 +1,22 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 class Barista {
 
-    public static void main(String[] args) {
-        String baseInputDirectoryName = "src/main/resources/templates/barista";
-        String baseOutputDirectoryName = "src/main/java";
+    static String baseInputDirectoryName = "src/main/resources/templates/barista";
+    static String baseOutputDirectoryName = "src/main/java";
+    final static String LINE = "\n";
 
-        System.out.println("Hello dear customer :)");
-        System.out.println("What can I do for you today?");
-        System.out.println("1 - I want an entity");
-        System.out.println("2 - I want a controller");
+    public static void main(String[] args) {
+        
+        System.out.println(
+            "Hello dear customer :)"
+            + LINE + "What can I do for you today?"
+            + LINE + "1 - I want an entity"
+            + LINE + "2 - I want a controller"
+        );
 
         Scanner scanner = new Scanner(System.in);
 
@@ -24,50 +29,35 @@ class Barista {
             e.printStackTrace();
         }
 
-        String outputDirectoryName = baseOutputDirectoryName;
-
         switch(choice) {
             case 1:
-                outputDirectoryName += "/entities";
+                baseOutputDirectoryName += "/entities";
                 System.out.println("Do you need an entity? Just tell its name:");
 
                 String entityName = scanner.nextLine();
+                if(Character.isLowerCase(entityName.charAt(0))) {
+                    System.out.println("First letter must be upper case for class name. Nevertheless,");
+                    break;
+                }
 
                 try {
-                    String fileContent = new String(
-                        Files.readAllBytes(
-                            Paths.get(baseInputDirectoryName + "/Entity.java")
-                        )
-                    );
-        
-                    String newFileContent = fileContent.replace("${Entity}", entityName);
-        
-                    System.out.println("Writing " + entityName + ".java for you...");
-                    Files.createDirectories(Paths.get(outputDirectoryName));
-                    Files.write(Paths.get(outputDirectoryName + "/" + entityName + ".java"), newFileContent.getBytes());
+                    String fileContent = getRenamedClassFileContent("Entity", entityName);
+                    generateClassFile(fileContent, entityName);
                 }
                 catch(Exception e) {
                     e.printStackTrace();
                 }
                 break;
             case 2:
-                outputDirectoryName += "/controllers";
+                baseOutputDirectoryName += "/controllers";
                 System.out.println("Do you need a resource controller? Just tell the name of your resource:");
 
                 String resourceName = scanner.nextLine();
 
                 try {
-                    String fileContent = new String(
-                        Files.readAllBytes(
-                            Paths.get(baseInputDirectoryName + "/Controller.java")
-                        )
-                    );
-        
-                    String newFileContent = fileContent.replace("${Resource}", resourceName);
-        
-                    System.out.println("Writing " + resourceName + "Controller.java for you...");
-                    Files.createDirectories(Paths.get(outputDirectoryName));
-                    Files.write(Paths.get(outputDirectoryName + "/" + resourceName + "Controller.java"), newFileContent.getBytes());
+                    String fileContent = getRenamedClassFileContent("Controller", resourceName);
+                    resourceName += "Controller";
+                    generateClassFile(fileContent, resourceName);
                 }
                 catch(Exception e) {
                     e.printStackTrace();
@@ -81,4 +71,21 @@ class Barista {
 
         System.out.println("Have a nice day :)");
     }
+
+    private static String getRenamedClassFileContent(String fileName, String className) throws IOException {
+        return new String(
+            Files.readAllBytes(
+                Paths.get(baseInputDirectoryName + "/" + fileName + ".java")
+            )
+        ).replace("${ClassName}", className);
+    }
+
+    private static void generateClassFile(String content, String fileName) throws IOException {
+        fileName += ".java";
+           
+        System.out.println("Writing " + fileName + " for you...");
+        Files.createDirectories(Paths.get(baseOutputDirectoryName));
+        Files.write(Paths.get(baseOutputDirectoryName + "/" + fileName), content.getBytes());
+    }
+
 }
