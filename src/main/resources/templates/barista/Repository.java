528bdf0ed0879006e1c54450195c0dfc0b6395db
Malvikabhalla99 <malvikabhalla99@gmail.com>
2019-10-02 17:25:53 +0530

    Repository updated

--- src/main/resources/templates/barista/Repository.java
+++ src/main/resources/templates/barista/Repository.java
@@ -1,8 +1,14 @@
+package #####;
 
-public interface Repository extends JpaRepository<Foo,Long>{
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+/* import ${Repository} Entity here */
+
+@Repository
+public interface ${Repository}Repository extends JpaRepository<${Repository},Long>{
 	
 	//Finding foo by id
-	public Foo findById(Long id);
+	public ${Repository} findById(Long id);
 	
 	//Deleting by id
 	public void deleteById(Long id);
