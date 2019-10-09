125959a7c4caa919bd2eed508900d83c9ca74a47
chaturanga <chaturanga@huubap.com>
2019-10-10 02:24:10 +0800

    #comment added date properties to entity template

--- src/main/resources/templates/barista/Entity.java
+++ src/main/resources/templates/barista/Entity.java
@@ -1,15 +1,22 @@
 package #####;
 
-import javax.persistence.Entity;
-import javax.persistence.GenerationType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class ${Entity} {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateCreated;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateModified;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateDeleted;
 
     public Long getId() {
         return id;
@@ -18,4 +25,33 @@ public class ${Entity} {
     public void setId(Long id) {
         this.id = id;
     }
+
+    @PrePersist
+    void onPrePersist() {
+        this.dateCreated = new Date();
+        this.dateModified = new Date();
+    }
+
+    @PreUpdate
+    void onPreUpdate() {
+        this.dateModified = new Date();
+    }
+
+    @PreRemove
+    void onPreRemove() {
+        this.dateDeleted = new Date();
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public Date getDateModified() {
+        return dateModified;
+    }
+
+    public Date getDateDeleted() {
+        return dateDeleted;
+    }
+
 }
